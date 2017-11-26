14dc6de44115c9215a33d3f00017c2fec9a303c3
Thibaud <thibaud@debian>
2017-11-26 12:51:40 +0100

    Add tests (using CTest)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <tuple>
+#include <cassert>
 
 #include "Magic.hpp"
 
@@ -8,21 +10,39 @@ using namespace FileTyping;
 
 int main(int argc, char** argv)
 {
+  const string filepath(argv[1]);
+
   cout << ":: File typing" << endl;
+  cout << "::\t" << filepath << endl;
 
-  const std::string filepath(argv[1]);
+  auto check = [&](auto&& first, auto&& second, const string& exc) noexcept {
+    if(first != second)
+      throw runtime_error(exc);
+  };
 
-  Magic m;
-  if(m.open(filepath)) {
-    cout << "Mime : " << m.mime() << endl;
-    cout << "Type : " << m.type() << endl;
-    cout << "Format : " << m.format() << endl;
-    cout << "Flags : 0x" << setfill('0') << setw(6) << hex << m.flags() << endl;
-  } else {
-    cout << "Error : " << m.error() << endl;
+  try {
+    Magic m;
+    m.open(filepath);
+
+    cout << "\t\tUsed flags : 0x" << setfill('0') << setw(6) << hex << m.flags() << endl;
+
+    string type, format;
+    std::tie(type, format) = Magic::type(filepath);
+
+    check(type, m.type(), "Type detection failed");
+    check(format, m.format(), "Format detection failed");
+
+    if(argc > 2) {
+      const string cmpMimetype(argv[2]);
+      check(cmpMimetype, m.mime(), "Mimetype comparison failed");
+    }
+
+    cout << "\t\tMime type : " << m.mime() << endl;
+
+  } catch(const runtime_error& re) {
+    cout << "\t\tFile typing error : " << re.what() << endl;
+    return EXIT_FAILURE;
   }
 
-  const auto tf = Magic::type(filepath);
-
-  return 0;
+  return EXIT_SUCCESS;
 };
